239d395ede073cf282db5fdfebe5788734297a5a
KevinGage <dudeman02379@gmail.com>
2022-06-02 09:23:13 -0400

    completed f to c test

--- fahrenheit_to_celsius/src/main.rs
+++ fahrenheit_to_celsius/src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Input a temperature in fahrenheit");
+
+    let mut temp_f = String::new();
+
+    io::stdin()
+        .read_line(&mut temp_f)
+        .expect("Failed to read line");
+
+    let temp_f: f32 = temp_f.trim().parse().expect("Temperature must be a number");
+
+    println!("Temp in f: {}", temp_f);
+
+    let temp_c = (temp_f - 32.0) / 1.8;
+
+    println!("Temp in c: {}", temp_c);
 }
