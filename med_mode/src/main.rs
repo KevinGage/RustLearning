535b20d7129c27c67b29d423a3c1a3b608cf5f6e
KevinGage <dudeman02379@gmail.com>
2022-06-03 15:17:51 -0400

    bootstrapped new project

---
+++ med_mode/src/main.rs
@@ -0,0 +1,11 @@
+// Given a list of integers,
+// use a vector and return the median (when sorted, the value in the middle position)
+// and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+
+fn get_median() {}
+
+fn get_mode() {}
+
+fn main() {
+    println!("Hello, world!");
+}
