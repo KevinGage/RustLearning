adcbb5742e2c0a18ab4b5b8a9d1275ca95875d45
KevinGage <dudeman02379@gmail.com>
2022-06-09 11:23:57 -0400

    implemented thread pool and workers for multi threading

--- tutorial_web_server/src/bin/main.rs
+++ tutorial_web_server/src/bin/main.rs
@@ -2,9 +2,14 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+use tutorial_web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    // Max of 4 threads so system can't be DOSed
+    let pool = ThreadPool::new(4);
 
     // For each connection attempt to listener
     for stream in listener.incoming() {
@@ -12,8 +17,9 @@ fn main() {
 
         println!("Connection established!");
 
-        handle_connection(stream);
-
+        pool.execute(|| {
+            handle_connection(stream);
+        });
         // Connection is closed because it goes out of scope
     }
 }
@@ -26,9 +32,14 @@ fn handle_connection(mut stream: TcpStream) {
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    // If GET request for /
     let (status_line, filename) = if buffer.starts_with(get) {
+        // If GET request for /
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        // If GET request for /sleep simulate work
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
---
+++ tutorial_web_server/src/lib.rs
@@ -0,0 +1,67 @@
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+// Type alias so no need to type Box<dyn FnOnce() + Send + 'static>; all the time
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        // wrap receiver in Mutex to make it threadsafe so it can be passed to all worker threads
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    // Takes a closure that impliments FnOnce, returns (), implements Send, and is static
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+pub struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {} got a job; executing.", id);
+
+            job();
+        });
+
+        Worker { id, thread }
+    }
+}
